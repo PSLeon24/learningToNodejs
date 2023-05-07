4a120ea462605cfe5d78cb0788084227f4ae6cb2
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-05-08 03:49:50 +0900

    Practice creating animal sounds API

--- index.js
+++ index.js
@@ -6,23 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/animals/:id', (req, res) => {
-    // 1. Routing by parameter value
-    // const q = req.params
-    // console.log(q)
-    // if (q.id === 'cat') {
-    //     res.send({'고양이': '야옹~'})
-    // } else if (q.id === 'dog') {
-    //     res.send({'강아지': '멍멍!'})
-    // } else {
-    //     res.send('올바른 동물 id를 입력하시오!')
-    // }
+app.get('/sounds/:name', (req, res) => {
+    const { name }= req.params
 
-    // 2. Get values by query
-    const q = req.query
-    console.log(q)
-    res.json({'id': q.id,
-            'age': q.age})
+    if(name === "dog"){
+        res.json({'sound': '멍멍'})
+    } else if(name === 'cat') {
+        res.json({'sound': '야옹'})
+    } else if(name === 'pig') {
+        res.json({'sound': '꿀꿀'})
+    } else {
+        res.json({'sound': '알 수 없음'})
+    }
 })
 
 app.listen(port, () => {
