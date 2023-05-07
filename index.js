3f262f1caa66ca3b6413822f6c98b9e1e3e8c00c
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-05-08 03:30:00 +0900

    Routing by parameter values

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/dog', (req, res) => {
-    res.send({'강아지': '멍멍!'})
+app.get('/animals/:id', (req, res) => {
+    const q = req.params
+    console.log(q)
+    if (q.id === 'cat') {
+        res.send({'고양이': '야옹~'})
+    } else if (q.id === 'dog') {
+        res.send({'강아지': '멍멍!'})
+    } else {
+        res.send('올바른 동물 id를 입력하시오!')
+    }
 })
-
-app.get('/cat', (req, res) => {
-    res.json({'고양이': '야옹~'})
-  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
