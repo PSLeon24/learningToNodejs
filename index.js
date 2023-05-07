01b3073bb956afe3c7c3c293a05b43ceafd0abf6
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-05-08 01:52:39 +0900

    practice figlet by nodejs

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello world!')
+var figlet = require("figlet");
+
+figlet("PSLeon24", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
