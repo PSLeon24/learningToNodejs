066fed367a41d5dbc0b0e1ce2f095a3a1b6c6a45
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-05-08 03:41:10 +0900

    Get values by query

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ app.get('/', (req, res) => {
 })
 
 app.get('/animals/:id', (req, res) => {
-    const q = req.params
+    // 1. Routing by parameter value
+    // const q = req.params
+    // console.log(q)
+    // if (q.id === 'cat') {
+    //     res.send({'고양이': '야옹~'})
+    // } else if (q.id === 'dog') {
+    //     res.send({'강아지': '멍멍!'})
+    // } else {
+    //     res.send('올바른 동물 id를 입력하시오!')
+    // }
+
+    // 2. Get values by query
+    const q = req.query
     console.log(q)
-    if (q.id === 'cat') {
-        res.send({'고양이': '야옹~'})
-    } else if (q.id === 'dog') {
-        res.send({'강아지': '멍멍!'})
-    } else {
-        res.send('올바른 동물 id를 입력하시오!')
-    }
+    res.json({'id': q.id,
+            'age': q.age})
 })
 
 app.listen(port, () => {
