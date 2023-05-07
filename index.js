433754f532b28ddd3ebda35db3519ad79f022d7e
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-05-08 03:24:11 +0900

    Create an example API

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-var figlet = require("figlet");
+const express = require('express')
+const app = express()
+const port = 3000
 
-figlet("PSLeon24", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/dog', (req, res) => {
+    res.send({'강아지': '멍멍!'})
+})
+
+app.get('/cat', (req, res) => {
+    res.json({'고양이': '야옹~'})
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
